d941a1b4749d0fddd7e94f8dbb22c451eaa55774
Garrett Fermo <garrettcfermo@gmail.com>
2018-12-07 13:00:26 -0800

    Boiler Plate Routes File

---
+++ routes/percentages.js
@@ -0,0 +1,43 @@
+const db = require('../models/percentages')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const moment = require('moment')
+
+
+module.exports = app => {
+  // app.get('/surfboards', (req, res) => {
+  //   db.surfboards.findAll({})
+  //     .then(r => res.json(r))
+  //     .catch(e => console.log(e))
+  // })
+
+  // app.get('/surfboards/:id', (req, res) => {
+  //   db.surfboards.findOne({ where: { id: req.params.id } })
+  //     .then(r => res.json(r))
+  //     .catch(e => console.log(e))
+  // })
+
+  // app.post('/surfboards', (req, res) => {
+  //   db.surfboards.create(req.body)
+  //     .then(() => res.sendStatus(200))
+  //     .catch(e => console.log(e))
+  // })
+
+  // app.put('/surfboards/:id', (req, res) => {
+  //   db.surfboards.update(req.body, { where: { id: req.params.id } })
+  //     .then(() => res.sendStatus(200))
+  //     .catch(e => console.log(e))
+  // })
+
+  // app.delete('/surfboards/:id', (req, res) => {
+  //   db.surfboards.destroy({ where: { id: req.params.id } })
+  //     .then(() => res.sendStatus(200))
+  //     .catch(e => console.log(e))
+  // })
+
+  // app.delete('/surfboards', (req, res) => {
+  //   db.surfboards.destroy({ where: {}, truncate: true })
+  //     .then(() => res.sendStatus(200))
+  //     .catch(e => console.log(e))
+  // })
+}
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
 const path = require('path')
-const axios = require('axios')
-const cheerio = require('cheerio')
-const moment = require('moment')
-
+const mongoose = require('mongoose')
 
 const app = express()
 mongoose.connect('mongodb://localhost/fuelhistorydb', { useNewUrlParser: true })
@@ -14,6 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json())
 
-var db = require("./models/percentages");
+
+require('./routes/percentages')(app)
 
 app.listen(process.env.PORT || 3000, _ => console.log('http://localhost:3000'))
